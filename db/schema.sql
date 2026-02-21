706b031bc1623e3076982ee89c9d56506679099a
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2026-02-21 16:10:04 +0000

    feat: add original_bloom_id column to blooms table for rebloom support

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE blooms (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     sender_id INT NOT NULL REFERENCES users(id),
     content TEXT NOT NULL,
-    send_timestamp TIMESTAMP NOT NULL
+    send_timestamp TIMESTAMP NOT NULL,
+    original_bloom_id BIGINT REFERENCES blooms(id)
 );
 
 CREATE TABLE follows (
