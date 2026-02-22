8369928a452c410c3715b5c4512b5ea4a4a08a70
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2026-02-22 11:57:23 +0000

    feat: add data-layer unfollow function

--- backend/data/follows.py
+++ backend/data/follows.py
@@ -21,6 +21,15 @@ def follow(follower: User, followee: User):
             pass
 
 
+    def unfollow(follower: User, followee: User):
+
+        with db_cursor() as cur:
+            cur.execute(
+                "DELETE FROM follows WHERE follower = %(follower_id)s AND followee = %(followee_id)s",
+                dict(follower_id=follower.id, followee_id=followee.id),
+            )
+
+
 def get_followed_usernames(follower: User) -> List[str]:
     """get_followed_usernames returns a list of usernames followee follows."""
     with db_cursor() as cur:
